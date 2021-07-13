68a31a8792e70b391cf101200b67cc6fbac6341d
Maksim <maksim@wsb-118-72b.local>
2021-07-13 15:54:58 +0300

    add model, view, controller User and gem devise

---
+++ db/migrate/20210713123719_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :bio
+      t.string :location
+      t.timestamps
+    end
+  end
+end
