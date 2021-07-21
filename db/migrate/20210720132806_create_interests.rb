265013ec1acbdf23a5c3a4f1a60f0216121ef5a3
Maksim <maksim@wsb-118-72b.local>
2021-07-21 10:13:57 +0300

    add model and controller Interest

---
+++ db/migrate/20210720132806_create_interests.rb
@@ -0,0 +1,9 @@
+class CreateInterests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :interests do |t|
+      t.string :type
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
